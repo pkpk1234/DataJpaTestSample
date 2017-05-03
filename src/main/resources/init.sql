30c79cc0e181e48b9c2c1724920f88b100133777
lijiaming <lijiaming1984@gmail.com>
2017-05-03 21:52:32 +0800

    delete sql files

--- src/main/resources/init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Person(first_name,last_name,age) VALUES ('li','jiaming',33);
-INSERT INTO Person(first_name,last_name,age) VALUES ('wang','chaoli',30);
-INSERT INTO Person(first_name,last_name,age) VALUES ('li','wangyu',2);
--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE person (
-  person_id  NUMBER AUTO_INCREMENT,
-  first_name VARCHAR2(256),
-  last_name  VARCHAR2(256),
-  age        INT,
-  PRIMARY KEY (person_id)
-);
